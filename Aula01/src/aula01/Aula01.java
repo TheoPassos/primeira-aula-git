0ffa53247c0ab68691e234adcd69e35e4ba0a8db
THEOFILO GABRIEL BRITO PASSOS <theofilo_passos@estudante.sesisenai.org.br>
2024-05-21 20:54:58 -0300

    Meu primeiro commit

---
+++ Aula01/src/aula01/Aula01.java
@@ -0,0 +1,19 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+       int idade = 19;
+       
+       int numeroCasa = Integer.parseInt("123");
+       
+       String nome = "Theofilo Gabriel Brito Passos";
+       boolean eAluno = true;
+       char genero = 'M';
+       float salario = 100.5f;
+       long idade2 = 26L;
+       
+
+       
---
+++ aula02.js
@@ -0,0 +1,10 @@
+var nome = "Theofilo Gabriel Brito Passos"
+var idade = 19
+var eAluno = true
+var dataNascimento
+
+document.write(typeof nome, "<br/>")
+document.write(typeof dataNascimento, "<br/>")
+document.write(typeof eAluno)
+
+
